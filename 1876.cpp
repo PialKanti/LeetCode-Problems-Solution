5635acd70b08f2d29c930579576df81a96f61a76
PialKanti <pialkanti2012@gmail.com>
2022-10-19 11:53:14 +0600

    Solved 1876

--- 1876.cpp
+++ 1876.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <unordered_set>
 using namespace std;
 
 int countGoodSubstrings(string s)
 {
     int result = 0;
-    unordered_set<char> window;
     for (int low = 0, high = 0; high < s.size(); high++)
     {
-        window.insert(s[high]);
         if (high - low == 2)
         {
-            if (window.size() == 3)
+            if (s[low] != s[low + 1] && s[low + 1] != s[high] && s[high] != s[low])
             {
                 result++;
             }
-            window.erase(s[low]);
             low++;
         }
     }
