4ccca06b649ad4d237974b37a59570b4c6b95294
PialKanti <pialkanti2012@gmail.com>
2023-01-29 15:14:16 +0600

    Solved 121. Best Time to Buy and Sell Stock

---
+++ 121.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxProfit(vector<int> &prices)
+{
+    int minPrice = prices[0];
+    int profit = 0;
+
+    for (int i = 0; i < prices.size(); i++)
+    {
+        if (prices[i] < minPrice)
+        {
+            minPrice = prices[i];
+        }
+
+        profit = max(profit, prices[i] - minPrice);
+    }
+
+    return profit;
+}
