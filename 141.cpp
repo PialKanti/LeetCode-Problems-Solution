6f3a370944d02f8063257c9f8d1399f43ec0de4a
PialKanti <pialkanti2012@gmail.com>
2023-02-12 12:36:15 +0600

    Solved 141. Linked List Cycle

---
+++ 141.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (slow && fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
